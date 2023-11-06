f8ec3c2dcc319f1227cfee37ecdefbeaafa993c7
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-11-06 10:32:19 +0700

    2023_11_06_10_32_19_122018

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,15 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('email')
+@ApiTags('Chức năng email')
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Get('test')
+  @ApiOperation({
+    summary: 'Thử chức năng email',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Email đã được gửi',
+  })
   async testSendEmail() {
     await this.emailService.testSendEmail();
     return {
-      message:   "Email đã được gửi" ,
+      message: 'Email đã được gửi',
     };
   }
 }
