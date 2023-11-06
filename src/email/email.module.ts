cd340ddf6172c282252b38ac7c22ad75b9d7952c
VuVanNghia <nghiavu2k2abc@gmail.com>
2023-11-06 11:06:55 +0700

    2023_11_06_11_06_55_410148

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -3,16 +3,17 @@ import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule } from '@nestjs/config';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MailerModule.forRoot({
       transport: {
-        // host: process.env.EMAIL_HOST,
-        // port: parseInt(process.env.EMAIL_PORT),
-        // secure: parseInt(process.env.EMAIL_PORT) === 465,
-        service: process.env.EMAIL_SERVICE_NAME,
+        host: process.env.EMAIL_HOST,
+        port: parseInt(process.env.EMAIL_PORT),
+        secure: parseInt(process.env.EMAIL_PORT) === 465,
+        // service: process.env.EMAIL_SERVICE_NAME,
         auth: {
           user: process.env.EMAIL_USER,
           pass: process.env.EMAIL_PASS,
@@ -21,9 +22,17 @@ import { ConfigModule } from '@nestjs/config';
       defaults: {
         from: process.env.EMAIL_USER,
       },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
     }),
   ],
   controllers: [EmailController],
   providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {}
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,17 +1,43 @@
-import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class EmailService {
   constructor(private readonly mailerService: MailerService) {}
-  async testSendEmail() {
-    const currentDateTime = new Date().toLocaleString();
+
+  async sendEmail1() {
     await this.mailerService.sendMail({
-      to: process.env.EMAIL_USER,
+      from: process.env.EMAIL_USER,
+      // to: 'nghia.vv2026205@sis.hust.edu.vn',
+        to: process.env.EMAIL_USER,
       // to: process.env.EMAIL_USER,
-      subject: currentDateTime,
-      text: 'This is a test email',
+      subject: '(Tổng cục thuế Demo) 1111111111111111111',
+      template: 'email1',
+      context: {
+        TenNguoiNopThue: 'Vũ Văn Nghĩa',
+        MaSoThue: '8693324674',
+        bien1: 'Giá trị biến bien1',
+      },
     });
-    return { message: 'Email đã được gửi' };
+  }
+  async sendEmail2() {
+    await this.mailerService.sendMail({
+      from: process.env.EMAIL_USER,
+      // to: 'nghia.vv2026205@sis.hust.edu.vn',
+        to: process.env.EMAIL_USER,
+      // to: process.env.EMAIL_USER,
+      subject: '(Tổng cục thuế Demo) sendEmail2',
+      template: 'email2',
+      context: {
+        TenNguoiNopThue: 'Vũ Văn Nghĩa',
+        MaSoThue: '8693324674',
+        bien21: 'Giá trị biến bien21',
+        bien22: 'Giá trị biến bien22',
+      },
+    });
+  }
+  async testSendEmail() {
+    await this.sendEmail1();
+    await this.sendEmail2();
   }
 }
